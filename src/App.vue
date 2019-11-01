ef0d70d8613cdd85e39e5b939dd672963c4a8f02
Willian Alves <wba.alves@gmail.com>
2019-11-01 00:45:51 -0300

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" style="background-color: steelblue;">
+    <NavBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "@/components/NavBar";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    NavBar
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar is-dark" role="navigation">
+    <span class="navbar-item">
+      <img src="@/assets/navbar-logo.png" />
+    </span>
+    <div class="navbar-item">
+      <button class="button is-primary">New post</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>

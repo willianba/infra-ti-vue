855cfa2adfcc8e4131b03a16bc6b3ce022447af7
Willian Alves <wba.alves@gmail.com>
2019-11-20 22:48:00 -0300

    create message bar component

---
+++ src/components/MessageBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    v-if="message"
+    class="message is-small"
+    :class="{ 'is-danger': error, 'is-success': !error }"
+    style="margin: 10px"
+  >
+    <div class="message-header">
+      <p>{{ error ? "Failed" : "Success" }}</p>
+      <button
+        class="delete"
+        @click="
+          message = '';
+          error = false;
+        "
+      ></button>
+    </div>
+    <div class="message-body">{{ message }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MessageBar",
+  props: {
+    error: {
+      type: Boolean,
+      required: true
+    },
+    message: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>

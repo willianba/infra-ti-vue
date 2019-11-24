b59292962fdf365661b9bbf53d8bef2a2b1580f9
Willian Alves <wba.alves@gmail.com>
2019-11-24 20:15:55 -0300

    create image list file

---
+++ src/components/ImagesList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="content">
+    <figure class="image" v-for="image in images.data" :key="image">
+      <img :src="image" class="resized-image" />
+    </figure>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ImagesList",
+  data() {
+    return {
+      images: ""
+    };
+  },
+  methods: {
+    async getImages() {
+      this.images = await axios.get("http://localhost:3000/");
+    }
+  },
+  mounted() {
+    this.getImages();
+  }
+};
+</script>
+
+<style scoped>
+.resized-image {
+  width: 45%;
+  height: auto;
+}
+</style>
